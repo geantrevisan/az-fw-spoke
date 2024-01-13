1d290df3790d8151f0fbffa509b4fb549477ced0
geantrevisan <geankevin2@icloud.com>
2024-01-12 22:42:43 -0300

    Script extra

---
+++ extra/run.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+sudo apt update -y
+sudo apt install -y apache2 unzip wget
+sudo systemctl start apache2
+sudo systemctl enable apache2
+wget https://github.com/geantrevisan/az-fw-spoke/raw/main/extra/b3.zip
+rm -rf /var/www/html/index.html
+unzip b3.zip -d /var/www/html/
